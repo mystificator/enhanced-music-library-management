34dd5d2c59117205fe90cfd540c1398b92f28250
mystificator <vipultandon.business@gmail.com>
2024-12-26 13:29:03 +0530

    updated grammy field to number

--- mongodb/models/artist.js
+++ mongodb/models/artist.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 const Artist = new mongoose.Schema({
     artist_id: { type: String, default: uuidv4 },
     name: { type: String, required: true },
-    grammy: { type: Boolean, default: false },
+    grammy: { type: Number, default: 0, min: 0, max: 10 },
     hidden: { type: Boolean, default: false },
 });
 
